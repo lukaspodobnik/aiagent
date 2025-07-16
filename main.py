e3653dd47952e42ccd267e6d5cbbf5d2b6524c68
lukaspodobnik <lukas.podobnik@gmail.com>
2025-07-16 13:12:55 +0200

    Add user input (for the prompt) and cl-option --verbose (to print token usage). Copy/paste  calculator-project (finished test project for the aiagent).

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
-    print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <prompt>")
+        exit(1)
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    prompt = sys.argv[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
 
 
 if __name__ == "__main__":
